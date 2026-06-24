1deec79a8878c01184b048b92ac45c5f4e42da7b
dstengerv <dstengerv@gmail.com>
2026-06-24 15:49:32 -0400

    Create sw.js

---
+++ src/public/sw.js
@@ -0,0 +1,12 @@
+// Service worker for PWA installation support
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Simple pass-through fetch handler is sufficient for PWA installability requirements
+});
